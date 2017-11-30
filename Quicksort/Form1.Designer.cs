000ec354964d549657474924972ce961f6287c2c
jrobinson52 <31286997+jrobinson52@users.noreply.github.com>
2017-11-29 20:17:58 -0500

    Initial push, has method book wanted and method according to Wikipedia

--- Quicksort/Form1.Designer.cs
+++ Quicksort/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(661, 46);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOutput.Location = new System.Drawing.Point(12, 130);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(661, 46);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSort.Location = new System.Drawing.Point(254, 64);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(167, 58);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 188);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtInput);
+            this.MinimumSize = new System.Drawing.Size(703, 235);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnSort;
     }
 }
 
--- Quicksort/Form1.cs
+++ Quicksort/Form1.cs
@@ -16,5 +16,115 @@ namespace Quicksort
         {
             InitializeComponent();
         }
+
+        private int[] getList()
+        {
+            List<int> lst = new List<int>();
+
+            foreach (string s in txtInput.Text.Split(' ')) //entries are divided by a space
+                lst.Add(Convert.ToInt32(s));
+
+            return lst.ToArray();
+        }
+
+        private int[] quickSort(int[] elements, int left, int right)
+        {
+            int L = left, R = right;
+            int pivot = elements[(L + R) / 2];
+            int[] returnVal = elements;
+
+            while (L <= R)
+            {
+                while (returnVal[L] < pivot)
+                    L++;
+
+                while (returnVal[R] > pivot)
+                    R--;
+
+                if (L <= R) //swap L and R
+                {
+                    int tmp = returnVal[L];
+                    returnVal[L] = returnVal[R];
+                    returnVal[R] = tmp;
+
+                    L++;
+                    R--;
+                }
+            }
+
+
+            if (left < R)
+                returnVal = quickSort(elements, left, L);
+            if (L < right)
+                returnVal = quickSort(elements, R, right);
+
+            return returnVal;
+        }
+
+        private int[] quickSortWiki(int[] elements, int left, int right)
+        {
+            if (left < right)
+            {
+                int p = partitionWiki(elements, left, right);
+                quickSortWiki(elements, left, p);
+                quickSortWiki(elements, p + 1, right);
+            }
+            return elements;
+        }
+
+        private int partitionWiki(int[] elements, int left, int right)
+        {
+            int pivot = elements[left];
+            int i = left;
+            int j = right;
+
+            while (1 == 1)
+            {
+                while (elements[i] < pivot)
+                {
+                    i = i + 1;
+                }
+
+                while (elements[j] > pivot)
+                {
+                    j = j - 1;
+                }
+
+                if (i >= j)
+                {
+                    return j;
+                }
+
+                int tmp = elements[i];
+                elements[i] = elements[j];
+                elements[j] = tmp;
+            }
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int[] unsorted = getList();
+
+            //  int[] sorted = quickSort(unsorted, 0, unsorted.Length - 1);
+
+            int[] sorted = quickSortWiki(unsorted, 0, unsorted.Length - 1);
+
+            //after sorting write to lbl
+            foreach (int i in sorted)
+                lblOutput.Text += i.ToString() + " ";
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            //prevents typing keys other than space or numbers
+            string output = "";
+            foreach (char c in txtInput.Text)
+                if (char.IsDigit(c) || c == ' ')
+                    output += c;
+            txtInput.Text = output;
+
+            txtInput.SelectionStart = txtInput.Text.Length; //moves cursor to end Without this cursor moves to beginning
+        }
     }
 }
